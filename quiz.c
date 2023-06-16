ec75a413ee515ef7137155a920176c36a7bd7e7b
Marcus com U <114195461+m4rcusml@users.noreply.github.com>
2023-06-15 23:16:44 -0400

    Update quiz.c

--- quiz.c
+++ quiz.c
@@ -5,16 +5,17 @@
 #include <math.h>
 #include <string.h>
 
-void mostrarQuestao(char enunciado[200], char a[100], char b[100], char c[100], char d[100], char e[100], int tempo)
-{
-	printf("Tempo: 00:%02d\n\n", tempo);
-    printf("%s\n\n", enunciado);
+void mostrarQuestao(char enunciado[200], char a[100], char b[100], char c[100], char d[100], char e[100], int tempo) {
+	printf(" Tempo: 00:%02d\n\n", tempo);
+    printf(" %s\n\n", enunciado);
 
-    printf("A) %s\n", a);
-    printf("B) %s\n", b);
-    printf("C) %s\n", c);
-    printf("D) %s\n", d);
-    printf("E) %s\n", e);
+    printf("  A) %s\n", a);
+    printf("  B) %s\n", b);
+    printf("  C) %s\n", c);
+    printf("  D) %s\n", d);
+    printf("  E) %s\n", e);
+    
+    printf("\n Responda: ");
 }
 
 void piscarTela(short certoErrado) {
@@ -36,21 +37,18 @@ void piscarTela(short certoErrado) {
 	}
 }
 
-void perguntar(char enunciado[200], char a[100], char b[100], char c[100], char d[100], char e[100], char respostaCorreta)
-{
+void perguntar(char enunciado[200], char a[100], char b[100], char c[100], char d[100], char e[100], char respostaCorreta) {
     char resp;
     int tempo = 30;
     int acerto = 0, erros = 0;
     int flag = 1, tentativas = 0;
 
-    while (flag == 1)
-    {
+    while (flag == 1) {
         mostrarQuestao(enunciado, a, b, c, d, e, tempo);
         Sleep(1000);
         tempo--;
 
-        if (tempo == 0)
-        {
+        if (tempo == 0) {
             system("cls");
             printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t    Seu tempo acabou!\n");
             piscarTela(0);
@@ -60,37 +58,30 @@ void perguntar(char enunciado[200], char a[100], char b[100], char c[100], char 
 
         system("cls");
 
-        if (kbhit())
-        {
+        if (kbhit()) {
             resp = getch();
 
-            if (resp == respostaCorreta)
-            {
+            if (resp == respostaCorreta) {
                 printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t  VOCÊ ACERTOU!!\n\n");
                 piscarTela(1);
                 acerto++;
                 flag = 0;
             }
-            else
-            {
-                if (tentativas >= 0 && tentativas < 3)
-                {
-                    if (resp == 'a' || resp == 'b' || resp == 'c' || resp == 'd' || resp == 'e')
-                    {
+            else {
+                if (tentativas >= 0 && tentativas < 3) {
+                    if (resp == 'a' || resp == 'b' || resp == 'c' || resp == 'd' || resp == 'e') {
                         printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t  RESPOSTA ERRADA!!!\n\n");
                         piscarTela(0);
                         flag = 0;
                         erros++;
                     }
-                    else
-                    {
+                    else {
                         printf("\n\n Tecla inválida, tente novamente.\n\n");
                         Sleep(2000);
                     }
                     system("cls");
                 }
-                else
-                {
+                else {
                     system("cls");
                     printf("\n \t\t\t\tMuitas tentativas, pare de tentar quebrar meu programa :(\n\n");
                     piscarTela(0);
@@ -105,8 +96,16 @@ void perguntar(char enunciado[200], char a[100], char b[100], char c[100], char 
     Sleep(500);
 }
 
-int main()
-{
-    perguntar("Qual é o resultado da expressão a seguir: 12 dividido por 3?", "4", "36", "123", "40", "6", 'a');
-    return 0;
+int main() {
+	setlocale(LC_ALL,"Portuguese");
+	
+	// tem que fazer o sistema de pontos
+	
+    perguntar("6. Qual o valor de X na equação: 2x + 5 = 13", "X = 4", "X = 6","X = 3","X = 2","X = 7", 'a');
+    perguntar("7. Qual o valor de X na equação: 3x - 8 = 10","X = 6","X = 9", "X = 4","X = 12","X = -1", 'a');
+    perguntar("8. Resolva: 4x^2 + 7x - 10 = 0","X = -2.24","X = 0.99","X = -3.5","X = 1.5","X = -0.76", 'a');
+    perguntar("9. Quais valores possíveis para |X|: v(3x + 2) = 5","(X = 6.23)","(X = 7.67)","(X = 1.45)","(X = -2.22)","(X = 2.22)", 'b');
+    perguntar("10. The last one... \n (x + 3)(2x - 5) = 0","(X = -3, X = 0)","(X = -3, X = 10)","(X = -3, X = -2.5)","(X = -3, X = 5/2)","(X = -3, X = 2.5)", 'd');
+	
+	return 0;
 }
