3cc9fa20ce7eb971bd9f7544f5a41c027cc8494a
DPFNeiland <107700177+DPFNeiland@users.noreply.github.com>
2023-06-16 20:27:34 -0400

    Add files via upload

---
+++ lOGO.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
+#include <stdlib.h>
+#define T 500
+
+void logo(){
+	system("color 6");
+	printf("\n\n\n\n\t\t%c-----------------%c\n",201,187);
+	Sleep(T);
+	system("cls");
+	printf("\n\n\n\n\t\t%c-----------------%c\n",201,187);
+	printf("\t\t|                 |\n");
+	Sleep(T);
+	system("cls");
+	printf("\n\n\n\n\t\t%c-----------------%c\n",201,187);
+	printf("\t\t|                 |\n");	
+	printf("\t\t|   Lusia's Quiz  |\n");
+	Sleep(T);
+	system("cls");
+	printf("\n\n\n\n\t\t%c-----------------%c\n",201,187);
+	printf("\t\t|                 |\n");	
+	printf("\t\t|   Lusia's Quiz  |\n");	
+	printf("\t\t|                 |\n");
+	Sleep(T);
+	system("cls");
+	printf("\n\n\n\n\t\t%c-----------------%c\n",201,187);
+	printf("\t\t|                 |\n");	
+	printf("\t\t|   Lusia's Quiz  |\n");	
+	printf("\t\t|                 |\n");	
+	printf("\t\t%c-----------------%c\n",200,188);
+}
+
+int main () {
+	
+	logo();
+	/*
+	printf("%c-----------------%c\n",201,187);
+	printf("|                 |\n");	
+	printf("|   Lusia's Quiz  |\n");	
+	printf("|                 |\n");	
+	printf("%c-----------------%c\n",200,188);
+	*/
+	
+	
+	
+	
+	
+}
